8ecf3f4459412e039304475a670651b60dcb0f8f
rangang <13452334233@163.com>
2024-10-17 17:52:42 +0800

    config6

--- main.js
+++ main.js
@@ -1,28 +1,76 @@
-const { app, BrowserWindow } = require('electron/main')
-const { updateElectronApp } = require('update-electron-app')
-updateElectronApp()
+const { app, BrowserWindow, dialog } = require('electron');
+const { autoUpdater } = require('electron-updater');
 
-const createWindow = () => {
-  const win = new BrowserWindow({
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
-  })
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
 
-  win.loadFile('index.html')
+  mainWindow.loadFile('index.html');
+
+  const currentVersion = app.getVersion();
+  dialog
+    .showMessageBox(mainWindow, {
+      type: 'info',
+      title: '第一次弹窗提示',
+      message: `提示1:当前版本是 ${currentVersion}，没有新版本可用。`
+    });
+
+  // 启动时检查更新
+  autoUpdater.checkForUpdates();
 }
 
-app.whenReady().then(() => {
-  createWindow()
+// 处理更新事件
+autoUpdater.on('update-available', (info) => {
+  const updateVersion = info.version;
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
+  // 弹窗提示用户
+  dialog
+    .showMessageBox(mainWindow, {
+      type: 'info',
+      title: '更新可用',
+      message: `发现新版本 ${updateVersion}，正在下载...`
+    })
+    .then(() => {
+      // 下载更新
+      autoUpdater.downloadUpdate();
+    });
+});
+
+autoUpdater.on('update-downloaded', () => {
+  dialog
+    .showMessageBox(mainWindow, {
+      type: 'info',
+      title: '更新下载完成',
+      message: '更新已下载，您可以重新启动应用程序以应用更新。'
+    })
+    .then(() => {
+      app.quit(); // 退出应用
+    });
+});
+
+// 提示当前版本号
+autoUpdater.on('update-not-available', () => {
+  const currentVersion = app.getVersion();
+  dialog
+    .showMessageBox(mainWindow, {
+      type: 'info',
+      title: '没有更新',
+      message: `当前版本是 ${currentVersion}，没有新版本可用。`
+    });
+});
+
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
